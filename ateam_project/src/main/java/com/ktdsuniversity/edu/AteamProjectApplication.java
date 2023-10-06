042e79f93c616ea485b8f6d7faaec8f2f3a6367f
shashak23 <lovemeten123@gmail.com>
2023-10-06 23:46:11 +0900

    231006_초기설정_시하

---
+++ ateam_project/src/main/java/com/ktdsuniversity/edu/AteamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ktdsuniversity.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteamProjectApplication.class, args);
+	}
+
+}
---
+++ ateam_project/src/test/java/com/ktdsuniversity/edu/AteamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktdsuniversity.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
