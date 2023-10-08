3047b6c8219dc7fdbfd681f63f600a1fb150d9fa
211-07\User <d>
2023-10-08 20:38:35 +0900

    beans package설정

---
+++ ateam_project/src/main/java/com/ktdsuniversity/edu/beans/CheckSessionInterceptor.java
@@ -0,0 +1,56 @@
+package com.ktdsuniversity.edu.beans;
+
+import java.util.Enumeration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ktdsuniversity.edu.member.vo.MemberVO;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class CheckSessionInterceptor implements HandlerInterceptor{
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		MemberVO memberVO = (MemberVO)session.getAttribute("_LOGIN_USER_");
+		if(memberVO == null) {
+			String method = request.getMethod().toLowerCase();
+			if(method.equals("get")) {
+				String requestURI = request.getRequestURI();
+				String queryString = getQueryString(request);
+				request.setAttribute("next",requestURI+queryString);
+			}
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/member/memberlogin.jsp");
+			rd.forward(request, response);
+			return false;
+		}
+		return true;
+	}
+	private String getQueryString(HttpServletRequest request) {
+		String queryString = "";
+		
+		Enumeration<String> parameterNames = request.getParameterNames();
+		String parameterName = null;
+		while(parameterNames.hasMoreElements()) {
+			parameterName = parameterNames.nextElement();
+			
+			if(queryString.equals("")) {
+				queryString="?";
+			}
+			else {
+				queryString +="&";
+			}
+			queryString += parameterName+ "=" +
+							request.getParameter(parameterName);
+		}
+		return queryString;
+	}
+	
+}
---
+++ ateam_project/src/main/java/com/ktdsuniversity/edu/beans/WebConfig.java
@@ -0,0 +1,49 @@
+package com.ktdsuniversity.edu.beans;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+@Configuration
+@Configurable 
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/views/", ".jsp");
+	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/js/**")
+		        .addResourceLocations("classpath:/static/js/");
+		registry.addResourceHandler("/css/**")
+        .addResourceLocations("classpath:/static/css/");
+		registry.addResourceHandler("/images/**")
+        .addResourceLocations("classpath:/static/images/");
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		//세션 체크를 하지 않을URL을 적어주세요.
+		
+		List<String> excludePatterns = new ArrayList<>();
+		excludePatterns.add("/member/signup/**");
+//		excludePatterns.add("/member/signup");
+		excludePatterns.add("/member/auth");
+		excludePatterns.add("/js/**");
+		excludePatterns.add("/css/**");
+		excludePatterns.add("/img/**");
+		
+		registry.addInterceptor(new CheckSessionInterceptor())
+				.addPathPatterns("/**")
+				.excludePathPatterns(excludePatterns);
+	}
+	
+}
