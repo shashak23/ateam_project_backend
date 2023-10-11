e7d78ce63ea112e77bc6fdc2e42485b82e87aee3
shashak23 <lovemeten123@gmail.com>
2023-10-11 14:06:53 +0900

    1010_main_커밋1

--- ateam_project/src/main/java/com/ktdsuniversity/edu/career/dao/CareerDAO.java
+++ ateam_project/src/main/java/com/ktdsuniversity/edu/career/dao/CareerDAO.java
@@ -3,5 +3,13 @@ package com.ktdsuniversity.edu.career.dao;
 import com.ktdsuniversity.edu.career.vo.CareerVO;
 
 public interface CareerDAO {
+	/**
+	 * 경력 생성
+	 */
 	public int createNewCareer(CareerVO careerVO);
+	
+	/**
+	 * 경력 수정
+	 */
+	public int updateOneCarrer(CareerVO careerVO);
 }
--- ateam_project/src/main/java/com/ktdsuniversity/edu/companynews/web/CompanyNewsController.java
+++ ateam_project/src/main/java/com/ktdsuniversity/edu/companynews/web/CompanyNewsController.java
@@ -1,5 +1,99 @@
 package com.ktdsuniversity.edu.companynews.web;
 
-public class CompanyNewsController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.ktdsuniversity.edu.companynews.service.CompanyNewsService;
+import com.ktdsuniversity.edu.companynews.vo.CompanyNewsListVO;
+import com.ktdsuniversity.edu.companynews.vo.CompanyNewsVO;
+
+@Controller
+public class CompanyNewsController {
+	
+	@Autowired
+	private CompanyNewsService companyNewsService;
+
+	@GetMapping("/news/view/{companyNewsPostId}") 
+	public String viewOneCompanyNews(@PathVariable String companyNewsPostId
+			                       , Model model) {
+		CompanyNewsVO companyNewsVO = companyNewsService
+				                     .getOneNews(companyNewsPostId, true);
+		model.addAttribute("companyNewsVO", companyNewsVO);
+		return "company/news/newsview";
+	}
+	
+	@GetMapping("/news/list")
+	public String viewCompanyNewsList(Model model) {
+		CompanyNewsListVO companyNewsListVO = companyNewsService.getAllCompanyNews();
+		
+		model.addAttribute("", companyNewsListVO);
+		return "company/news/newslist";
+	}
+	
+	@GetMapping("/news/create")
+	public String viewCompanyNewsCreatePage() {
+		return "company/news/newscreate";
+	}
+	
+	@PostMapping("/news/create")
+	public String doCompanyNewsCreate(@ModelAttribute CompanyNewsVO companyNewsVO
+			                        , Model model) {
+		
+		boolean isSuccess = companyNewsService.createNewCompanyNews(companyNewsVO);
+		if(isSuccess) {
+			return "redirect:/news/list";
+		}
+		else {
+			model.addAttribute("companyNewsVO", companyNewsVO);
+			return "company/news/newscreate";
+		}
+	}
+	
+	@GetMapping("/news/update/{companyNewsPostId}")
+	public String viewCompanyNewsUpdatePage(@PathVariable String companyNewsPostId
+			                              , Model model) {
+		System.out.println("PathVariable: " + companyNewsPostId);
+		
+		CompanyNewsVO companyNewsVO = companyNewsService.getOneNews(companyNewsPostId, false);
+		model.addAttribute("companyNewsVO", companyNewsVO);
+		return "company/news/newsupdate";
+	}
+	
+	@PostMapping("/news/update")
+	public String doCompanyNewsUpdate(@ModelAttribute CompanyNewsVO companyNewsVO
+			                        , Model model) {
+		System.out.println("Post ID: " + companyNewsVO.getCompanyNewsPostId());
+		System.out.println("제목: " + companyNewsVO.getPostTitle());
+		System.out.println("내용: " + companyNewsVO.getPostContent());
+		System.out.println("등록일: " + companyNewsVO.getPostDate());
+		
+		boolean isSuccess = companyNewsService.updateOneCompanyNews(companyNewsVO);
+		if(isSuccess) {
+			return "redirect:/news/view?companyPostId=" + companyNewsVO.getCompanyNewsPostId();
+		}
+		else {
+			model.addAttribute("companyNewsVO", companyNewsVO);
+			return "company/news/newsupdate";
+		}
+	}
+	
+	@PostMapping("/news/delete")
+	public String doDeleteCompanyNews(@ModelAttribute CompanyNewsVO companyNewsVO
+                                    , Model model) {
+		boolean isSuccess = companyNewsService.deleteOneCompanyNews(companyNewsVO);
+		if(isSuccess) {
+			return "redirect:/news/list";
+		}
+		else {
+			model.addAttribute("companyNewsVO", companyNewsVO);
+			return "redirect:/news/view?companyPostId=" + companyNewsVO.getCompanyNewsPostId();
+		}
+	}
 }
