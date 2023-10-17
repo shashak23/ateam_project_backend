61b83f606d75a79eba5c47acdffc7af947c9d417
211-09\User <qnprk55@gmail.com>
2023-10-17 13:44:31 +0900

    ㅎㅅㅎ

--- ateam_project/src/main/java/com/ktdsuniversity/edu/notice/web/NoticeController.java
+++ ateam_project/src/main/java/com/ktdsuniversity/edu/notice/web/NoticeController.java
@@ -8,7 +8,6 @@ package com.ktdsuniversity.edu.notice.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
