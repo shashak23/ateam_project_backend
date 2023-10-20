3a25ee861788ac79085f7e2b25646915e3f47fa9
BoniJang <bonniejang12@gmail.com>
2023-10-20 20:18:00 +0900

    작성자추가

--- ateam_project/src/main/java/com/ktdsuniversity/edu/algorithmcategorylist/dao/AlgorithmCategoryListDAOImpl.java
+++ ateam_project/src/main/java/com/ktdsuniversity/edu/algorithmcategorylist/dao/AlgorithmCategoryListDAOImpl.java
@@ -1,3 +1,8 @@
+/**
+ * 작성자: 장보늬
+ * 작성날짜: 2023-10-19
+ * 내용: 알고리즘문제가 가지는 카테고리 목록 DAOImpl
+ * **/
 package com.ktdsuniversity.edu.algorithmcategorylist.dao;
 
 import java.util.List;
--- ateam_project/src/main/java/com/ktdsuniversity/edu/algorithmcategorylist/web/AlgorithmCategoryListController.java
+++ ateam_project/src/main/java/com/ktdsuniversity/edu/algorithmcategorylist/web/AlgorithmCategoryListController.java
@@ -1,6 +1,10 @@
+/**
+ * 작성자: 장보늬
+ * 작성날짜: 2023-10-19
+ * 내용: 알고리즘문제가 가지는 카테고리 목록 컨트롤러
+ * **/
 package com.ktdsuniversity.edu.algorithmcategorylist.web;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
