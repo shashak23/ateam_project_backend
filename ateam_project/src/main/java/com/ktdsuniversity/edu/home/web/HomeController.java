1f418e8588cc31bf64c9461e65b9db6bbf818d84
211-09\User <qnprk55@gmail.com>
2023-10-23 18:13:34 +0900

    메인화면 컨트롤러 개발

--- ateam_project/src/main/java/com/ktdsuniversity/edu/home/web/HomeController.java
+++ ateam_project/src/main/java/com/ktdsuniversity/edu/home/web/HomeController.java
@@ -1,13 +1,36 @@
 package com.ktdsuniversity.edu.home.web;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ktdsuniversity.edu.generalpost.service.GeneralPostService;
+import com.ktdsuniversity.edu.generalpost.vo.GeneralPostVO;
 
 @Controller
 public class HomeController {
+	@Autowired
+	private GeneralPostService generalPostService;
 
 	@GetMapping("/home/home")
 	public String homeLink() {
 		return "home/home";
+	}
+	
+	@ResponseBody
+	@GetMapping("/home/maincontent")
+	public Map<String, Object> getAllMainContents() {
+		Map<String, Object> resultMap = new HashMap<>();
+		List<GeneralPostVO> generalList = new ArrayList<>();
+		generalList.addAll(generalPostService.getAllFreeBoard().getGeneralPostList());
+		generalList.addAll(generalPostService.getAllQnABoard().getGeneralPostList());
+		resultMap.put("contents", generalList);
+		return resultMap;
 	}
 }
